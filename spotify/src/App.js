dedca5c4222341573ded03a4af55582f2aac97d0
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-01-22 16:59:48 +0530

    added spotify web api

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -3,19 +3,26 @@ import Login from './components/Login';
 import { useEffect } from 'react';
 import { getTokenFormUrl } from './spotify';
 import { useState } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
+
 
 function App() {
-
   const [token,setToken] = useState(null);
 
   useEffect(()=>{
     const hash = getTokenFormUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-
     
     if(_token){
       setToken(_token);
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user =>{
+        console.log("user",user);
+      })
     }
 
     console.log("access spotify 🚀" , _token); 
