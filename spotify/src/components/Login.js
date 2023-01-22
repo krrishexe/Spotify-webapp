78a7c085e8939cbe3ea2292e8b477f48220d79da
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-01-22 11:20:03 +0530

    updated spotify redirecting page

--- spotify/src/components/Login.js
+++ spotify/src/components/Login.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import "../css/Login.css"
+import { loginUrl } from '../spotify'
 
 function Login() {
   return (
     <div className='login'>
       <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
 
-      <a href='/'>Login with Spotify</a>
-
+      <a href={loginUrl}>Login with Spotify</a>
 
 
     </div>
