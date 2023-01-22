b77b5c08c6ca287269043ea5dd67645f9395a4e3
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-01-22 22:58:37 +0530

    updated spotify js and added player js

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { getTokenFormUrl } from './spotify';
 import { useState } from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
+import Player from './components/Player';
 
 const spotify = new SpotifyWebApi();
 
@@ -15,23 +16,22 @@ function App() {
     const hash = getTokenFormUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-    
+
     if(_token){
       setToken(_token);
-      spotify.setAccessToken(_token);
-
-      spotify.getMe().then(user =>{
+      spotify.setAccessToken(_token); 
+      spotify.getMe().then(user =>{     // .then bcoz the getMe() method return a promise.
         console.log("user",user);
       })
     }
-
-    console.log("access spotify 🚀" , _token); 
+    
+    console.log("access spotify 🚀", _token); 
   },[]);
 
   return (
     <div className="app">
       {
-        token ? <h1>Congrats you are logged in !!</h1>:<Login/>
+        token ? <Player/> : <Login/>  // if the user comes with a access token in the url then run the player component otherwise run Login component again.
       }
 
     </div>
