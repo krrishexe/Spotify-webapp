dd9f5db4c63d5665076a123474f87921505d1558
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-01-22 12:44:07 +0530

    updated app js

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,11 +1,32 @@
 import './css/App.css';
 import Login from './components/Login';
+import { useEffect } from 'react';
+import { getTokenFormUrl } from './spotify';
+import { useState } from 'react';
 
 function App() {
+
+  const [token,setToken] = useState(null);
+
+  useEffect(()=>{
+    const hash = getTokenFormUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    
+    if(_token){
+      setToken(_token);
+    }
+
+    console.log("access spotify 🚀" , _token); 
+  },[]);
+
   return (
     <div className="app">
-      
-      <Login/>
+      {
+        token ? <h1>Congrats you are logged in !!</h1>:<Login/>
+      }
+
     </div>
   );
 }
--- spotify/src/spotify.js
+++ spotify/src/spotify.js
@@ -15,4 +15,17 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
+export const getTokenFormUrl = () =>{
+    return window.location.hash
+    .substring(1)
+    .split('&')
+    .reduce((inital,item) => {
+        var parts = item.split('=')
+        inital[parts[0]] = decodeURIComponent(parts[1])
+        return inital;
+    }, {});
+}
+
+
+
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
