804b3167493e1c7c98c7184c73e817e3562857cb
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-01-27 16:56:27 +0530

    updated sidebar and body css

--- spotify/src/components/Sidebar.js
+++ spotify/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "../css/Sidebar.css"
 
 function Sidebar() {
   return (
-    <div>
+    <div className='sidebar'>
       <h1>I am the Sidebar</h1>
     </div>
   )
