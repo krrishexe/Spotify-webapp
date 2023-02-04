9e8501fae05fbfef74dc3dc24d56fe5a8beb8483
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-02-04 11:11:02 +0530

    added svg icons

--- spotify/src/components/Sidebar.js
+++ spotify/src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../css/Sidebar.css"
-// import SidebarOption from './SidebarOption'
+import SidebarOption from './SidebarOption'
 // import HomeIcon from '@mui/icons-material/Home';
 // import SearchIcon from '@mui/icons-material/Search';
 // import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
@@ -10,9 +10,9 @@ function Sidebar() {
     <div className='sidebar'>
       <img className='sidebar__logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
 
-      {/* <SidebarOption title="Home" Icon={HomeIcon} />
-      <SidebarOption title="Search" Icon={SearchIcon} />
-      <SidebarOption title="Your Library" Icon={LibraryMusicIcon}  /> */}
+      <SidebarOption title="Home"  />
+      <SidebarOption title="Search" />
+      <SidebarOption title="Your Library" />
     </div>
   )
 }
