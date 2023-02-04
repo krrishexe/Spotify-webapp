f1025cd03f776e4601bb87e8be766f0b18319339
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-02-04 12:46:40 +0530

    fetching data from spotify web api

--- spotify/src/components/Sidebar.js
+++ spotify/src/components/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "../css/Sidebar.css"
 import SidebarOption from './SidebarOption'
 import home from "../media/icons8-home-page-24.png"
-import lib from  "../media/icons8-music-library-30.png"
+import lib from "../media/icons8-music-library-30.png"
 import search from "../media/icons8-search-30.png"
 // import HomeIcon from '@mui/icons-material/Home';
 // import SearchIcon from '@mui/icons-material/Search';
@@ -13,14 +13,18 @@ function Sidebar() {
     <div className='sidebar'>
       <img className='sidebar__logo' src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
 
-      <SidebarOption title="Home" Icon={home}  />
+      <SidebarOption title="Home" Icon={home} />
       <SidebarOption title="Search" Icon={search} />
       <SidebarOption title="Your Library" Icon={lib} />
       <br />
       <strong className='sidebar__title'>PLAYLISTS</strong>
       <hr />
+
+      <SidebarOption title="Your Library" />
+      <SidebarOption title="Your Library" />
+
     </div>
   )
 }
- 
+
 export default Sidebar
